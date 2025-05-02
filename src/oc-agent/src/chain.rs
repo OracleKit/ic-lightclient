d779f30afd479b8c448864e4298069646a837a4d
icnu <172075965+icnu@users.noreply.github.com>
2025-05-02 22:52:07 +0530

    refactor chain manager in oc agent (#7)

--- src/oc-agent/src/chain.rs
+++ src/oc-agent/src/chain.rs
@@ -1,4 +1,7 @@
+use std::sync::Arc;
+
 use ic_lightclient_types::{ChainState, ChainUpdates};
+use tokio::sync::Mutex;
 use crate::ethereum::EthereumChain;
 
 pub trait Chain {
@@ -9,14 +12,15 @@ pub trait Chain {
     async fn get_updates(&mut self, state: ChainState) -> Option<ChainUpdates>;
 }
 
+#[derive(Clone)]
 pub struct ChainManager {
-    pub ethereum: EthereumChain
+    pub ethereum: Arc<Mutex<EthereumChain>>
 }
 
 impl ChainManager {
     pub fn new() -> Self {
         Self {
-            ethereum: EthereumChain::new()
+            ethereum: Arc::new(Mutex::new(EthereumChain::new()))
         }
     }
 }
--- src/oc-agent/src/main.rs
+++ src/oc-agent/src/main.rs
@@ -4,10 +4,10 @@ mod ethereum;
 mod config;
 mod http;
 
-use std::{sync::Arc, time::Duration};
+use std::time::Duration;
 use chain::{Chain, ChainManager};
 use ic_lightclient_types::CanisterUpdates;
-use tokio::{sync::Mutex, time::sleep};
+use tokio::time::sleep;
 use icp::ICP;
 use config::load_config;
 
@@ -16,18 +16,16 @@ async fn main() {
     let config = load_config();
     ICP::init(config.icp).await;
 
-    let mut chain_manager = ChainManager::new();
-    chain_manager.ethereum.init(config.ethereum).await;
-    
-    let chain_manager = Arc::new(Mutex::new(chain_manager));
+    let chain_manager = ChainManager::new();
+    chain_manager.ethereum.try_lock().unwrap().init(config.ethereum).await;
 
     loop {
         let state = ICP::get_canister_state().await;
         let chain_manager = chain_manager.clone();
         
         tokio::spawn(async move {
-            let Ok(mut chain_manager) = chain_manager.try_lock() else { return };
-            let updates = chain_manager.ethereum.get_updates(state.ethereum).await;
+            let Ok(mut ethereum) = chain_manager.ethereum.try_lock() else { return };
+            let updates = ethereum.get_updates(state.ethereum).await;
 
             if let Some(updates) = updates {
                 ICP::update_canister_state(
