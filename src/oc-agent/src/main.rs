0596451c37facd8a950a8cc3471543987d77e51a
icnu <172075965+icnu@users.noreply.github.com>
2025-10-30 13:18:55 +0530

    more debug statements

--- src/oc-agent/src/main.rs
+++ src/oc-agent/src/main.rs
@@ -29,33 +29,42 @@ async fn main() -> Result<()> {
     println!("Received chains for configuration: {:?}", &configured_chains);
 
     for uid in configured_chains {
+        println!("Initializing chain: {}", uid);
         let config = ICP::get_canister_config(uid).await;
         let chain = build_chain_from_uid(uid);
         let mut chain = chain.lock().await;
         chain.init(config).await.unwrap();
+        println!("Initialized chain: {}", uid);
     }
 
     loop {
         let state = ICP::get_canister_state().await;
+        println!("Received canister state.");
+
         let state = Arc::new(StatePayloadParser::new(state).unwrap());
         let updates = Arc::new(Mutex::new(UpdatePayloadMarshaller::new()));
         let uids = chain_manager.list();
         let mut join_set = JoinSet::new();
 
         for uid in uids {
+            println!("Beginning sync for chain: {}", uid);
             let chain = chain_manager.get(&uid);
             let state = state.clone();
             let updates = updates.clone();
 
             join_set.spawn(async move {
+                println!("Spawned sync job for chain: {}", uid);
                 let mut chain = chain.lock().await;
                 let mut updates = updates.lock().await;
+                println!("Obtained locks for sync job for chain: {}", uid);
                 chain.get_updates(&state, &mut updates).await.unwrap();
                 println!("Ran updates for chain: {}", uid);
             });
         }
 
+        println!("Joining all spawned tasks.");
         join_set.join_all().await;
+        println!("Done.");
 
         let updates = updates.lock().await;
         if updates.has_updates() {
