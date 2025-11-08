7c7dea265e8dcbe03669267ec3d6e9f104c2dd07
icnu <172075965+icnu@users.noreply.github.com>
2025-11-08 19:01:39 +0530

    add support for batches in execution api (#43)

--- src/oc-agent/src/util/api/execution.rs
+++ src/oc-agent/src/util/api/execution.rs
@@ -1,8 +1,11 @@
+use std::{cell::RefCell, marker::PhantomData, rc::Rc};
+
 use crate::http::HttpClient;
 use alloy_primitives::U256;
 use alloy_rpc_types_eth::Header;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::value::RawValue;
 
 #[derive(Serialize)]
 struct JsonRpcRequestWrapper<T> {
@@ -54,6 +57,22 @@ impl ExecutionApi {
         Ok(response.result)
     }
 
+    pub async fn batch_request(&self, batch: ExecutionRequestBatch) -> Result<()> {
+        let url = &self.url;
+
+        let response = HttpClient::post(url)
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .json(&batch.collect_requests())
+            .send()
+            .await?;
+
+        let response = response.json().await?;
+        batch.process_response(response)?;
+
+        Ok(())
+    }
+
     #[allow(dead_code)]
     pub async fn latest_block_number(&self) -> Result<U256> {
         self.request("eth_blockNumber", ()).await
@@ -70,5 +89,85 @@ impl ExecutionApi {
 
     pub async fn max_priority_fee(&self) -> Result<U256> {
         self.request("eth_maxPriorityFeePerGas", ()).await
+    }
+}
+
+struct InnerReceipt {
+    method: String,
+    params: Box<RawValue>,
+    result: Rc<RefCell<Option<Box<RawValue>>>>,
+}
+
+pub struct ExecutionRequestBatch {
+    receipts: Vec<InnerReceipt>,
+}
+
+impl ExecutionRequestBatch {
+    pub fn new() -> Self {
+        Self { receipts: vec![] }
+    }
+
+    fn request<T, R>(&mut self, method: &str, params: T) -> Result<ExecutionRequestReceipt<R>>
+    where
+        T: Serialize,
+        R: DeserializeOwned,
+    {
+        let method = method.to_string();
+        let params = serde_json::to_string(&params)?;
+        let params = RawValue::from_string(params)?;
+        let result = Rc::new(RefCell::new(None));
+        let inner_receipt = InnerReceipt { method, params, result: result.clone() };
+        self.receipts.push(inner_receipt);
+
+        Ok(ExecutionRequestReceipt { result, _r: PhantomData })
+    }
+
+    fn collect_requests(&self) -> Vec<JsonRpcRequestWrapper<Box<RawValue>>> {
+        let requests = self
+            .receipts
+            .iter()
+            .enumerate()
+            .map(|(i, receipt)| JsonRpcRequestWrapper {
+                id: i as u32,
+                jsonrpc: "2.0".into(),
+                method: receipt.method.clone(),
+                params: receipt.params.clone(),
+            })
+            .collect();
+
+        requests
+    }
+
+    fn process_response(&self, responses: Vec<JsonRpcResponseWrapper<Box<RawValue>>>) -> Result<()> {
+        for response in responses {
+            let i = response.id;
+            let Some(receipt) = self.receipts.get(i as usize) else {
+                return Err(anyhow!("Invalid id received in response."));
+            };
+
+            receipt.result.replace(Some(response.result));
+        }
+
+        Ok(())
+    }
+
+    pub fn max_priority_fee(&mut self) -> Result<ExecutionRequestReceipt<U256>> {
+        self.request("eth_maxPriorityFeePerGas", ())
+    }
+}
+
+pub struct ExecutionRequestReceipt<T: DeserializeOwned> {
+    result: Rc<RefCell<Option<Box<RawValue>>>>,
+    _r: PhantomData<T>,
+}
+
+impl<T: DeserializeOwned> ExecutionRequestReceipt<T> {
+    pub fn get(&mut self) -> Result<T> {
+        let Some(raw) = self.result.take() else {
+            return Err(anyhow!("Result not populated."));
+        };
+
+        let parsed = serde_json::from_str(raw.get())?;
+        Ok(parsed)
     }
 }
